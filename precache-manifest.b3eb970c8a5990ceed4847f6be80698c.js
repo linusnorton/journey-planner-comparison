95a18b82aafb852269f1253589798a6fce9249b4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-14 13:18:36 +0000

    Deploy linusnorton/journey-planner-comparison to github.com/linusnorton/journey-planner-comparison.git:gh-pages

--- precache-manifest.b3eb970c8a5990ceed4847f6be80698c.js
+++ precache-manifest.b3eb970c8a5990ceed4847f6be80698c.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3094b79747530dc074164c96a1e2cbcb",
+    "revision": "ea56936cba8a0c1b40529d4ce2779415",
     "url": "./index.html"
   },
   {
-    "revision": "e6e75b2bf296f8e2fbd9",
+    "revision": "999ad274496c5c7d3396",
     "url": "./static/css/2.89f067aa.chunk.css"
   },
   {
-    "revision": "d1a90ac2d0cf311e862a",
+    "revision": "10185ab5dd283ac6cfb4",
     "url": "./static/css/main.6ef5b6cc.chunk.css"
   },
   {
-    "revision": "e6e75b2bf296f8e2fbd9",
-    "url": "./static/js/2.55b9316b.chunk.js"
+    "revision": "999ad274496c5c7d3396",
+    "url": "./static/js/2.10c3d8c2.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "./static/js/2.55b9316b.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.10c3d8c2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d1a90ac2d0cf311e862a",
-    "url": "./static/js/main.73286a91.chunk.js"
+    "revision": "10185ab5dd283ac6cfb4",
+    "url": "./static/js/main.b2e50c6c.chunk.js"
   },
   {
     "revision": "eaeedf938cc1575ae0a6",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.05d62edb411f22547cbc50e6a29d0abc.js"
+  "./precache-manifest.b3eb970c8a5990ceed4847f6be80698c.js"
 );
 
 self.addEventListener('message', (event) => {

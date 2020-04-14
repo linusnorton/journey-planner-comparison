484fb80cc19abba11331a70b9856a8391d704e53
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-14 11:23:37 +0000

    Deploy linusnorton/journey-planner-comparison to github.com/linusnorton/journey-planner-comparison.git:gh-pages

--- src/Util/time.ts
+++
@@ -1,7 +0,0 @@
-export function toTimeString(dateTime: string) {
-  return dateTime.split(" ").pop()!.substr(0, 5);
-}
-
-export function toMins(seconds: number) {
-  return Math.floor(seconds / 60) + " mins";
-}

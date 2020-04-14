8e9c9806a2afdf74033bb2fe349bfb8d41a7c40d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-14 11:34:26 +0000

    Deploy linusnorton/journey-planner-comparison to github.com/linusnorton/journey-planner-comparison.git:gh-pages

---
+++ precache-manifest.05d62edb411f22547cbc50e6a29d0abc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3094b79747530dc074164c96a1e2cbcb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e6e75b2bf296f8e2fbd9",
+    "url": "./static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "d1a90ac2d0cf311e862a",
+    "url": "./static/css/main.6ef5b6cc.chunk.css"
+  },
+  {
+    "revision": "e6e75b2bf296f8e2fbd9",
+    "url": "./static/js/2.55b9316b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.55b9316b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1a90ac2d0cf311e862a",
+    "url": "./static/js/main.73286a91.chunk.js"
+  },
+  {
+    "revision": "eaeedf938cc1575ae0a6",
+    "url": "./static/js/runtime-main.7004c11a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5feeb747d8098e86e4b369b5469c8e76.js"
+  "./precache-manifest.05d62edb411f22547cbc50e6a29d0abc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
